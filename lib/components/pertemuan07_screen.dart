ff06652a495d2b7f9578d6ab6a89cc8b8df1b1be
esibutarbutar <114061884+esibutarbutar@users.noreply.github.com>
2023-04-17 16:32:51 +0700

    Update pertemuan07_screen.dart

--- lib/components/pertemuan07_screen.dart
+++ lib/components/pertemuan07_screen.dart
@@ -1,97 +1,227 @@
-import 'package:app07/components/pertemuan07_provider.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/src/widgets/container.dart';
 import 'package:flutter/src/widgets/framework.dart';
-import 'package:flutter/src/widgets/placeholder.dart';
+import 'package:app06/pertemuan06/pertemuan06_provider.dart';
 import 'package:provider/provider.dart';
-import 'package:app07/components/pertemuan07_screen.dart';
-import 'package:app07/components/pertemuan07_body.dart';
 
-class Pertemuan07Screen extends StatefulWidget {
-  const Pertemuan07Screen({super.key});
+class Pertemuan06Screen extends StatefulWidget {
+  const Pertemuan06Screen({super.key});
 
   @override
-  State<Pertemuan07Screen> createState() => _Pertemuan07ScreenState();
+  State<Pertemuan06Screen> createState() => _Pertemuan06ScreenState();
 }
 
-class _Pertemuan07ScreenState extends State<Pertemuan07Screen> {
-  int _currentIndex = 0;
-  void onTabTapped(int index) {
-    setState(() {
-      _currentIndex = index;
-    });
+class _Pertemuan06ScreenState extends State<Pertemuan06Screen> {
+  String itemSelected = 'Pilih Pekerjaan';
+  String itemSelected1 = 'Pilih Cita-Cita';
+  List items = [
+    'Pilih Pekerjaan',
+    'Mahasiswa',
+    'Dosen',
+    'Programmer',
+    'UI UX Designer',
+    'IT Consultant',
+    'Project Manager',
+    'PNS',
+    'Wiraswasta'
+  ];
+
+  List items1 = [
+    'Pilih Cita-Cita',
+    'Programmer',
+    'Dosen',
+    'UI UX Designer',
+    'IT Consultant',
+  ];
+
+  @override
+  bool _status_switch = false;
+  Widget build(BuildContext context) {
+    final prov = Provider.of<Pertemuan06Provider>(context);
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(
+          'Switches | DropdownB.',
+        ),
+      ),
+      body: Column(
+        children: [
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Text('Dark mode theme',
+                  style: TextStyle(fontWeight: FontWeight.bold)),
+              Switch(
+                  value: prov.enableDarkMode,
+                  activeColor: Colors.green,
+                  onChanged: (val) {
+                    prov.setBrightness = val;
+                  })
+            ],
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              const Text(
+                'Pekerjaan',
+                style: TextStyle(fontWeight: FontWeight.bold),
+              ),
+              DropdownButton(
+                  items: items.map((item) {
+                    return DropdownMenuItem(
+                      child: Text(item),
+                      value: item,
+                    );
+                  }).toList(),
+                  value: itemSelected,
+                  onChanged: (val) {
+                    setState(() {
+                      itemSelected = val as String;
+                    });
+                  })
+            ],
+          ),
+          if (itemSelected == "Pilih Pekerjaan")
+            const Text("Anda Belum Memilih Pekerjaan")
+          else if (itemSelected == "Mahasiswa")
+            const Text("Pekerjaan : Mahasiswa")
+          else if (itemSelected == "Dosen")
+            const Text("Pekerjaan : Dosen")
+          else if (itemSelected == "Programmer")
+            const Text("Pekerjaan : Programmer")
+          else if (itemSelected == "UI UX Designer")
+            const Text("Pekerjaan : UI UX Designer")
+          else if (itemSelected == "IT Consultant")
+            const Text("Pekerjaan : IT Consultant")
+          else if (itemSelected == "Project Manager")
+            const Text("Pekerjaan : Project Manager")
+          else if (itemSelected == "PNS")
+            const Text("Pekerjaan : PNS")
+          else if (itemSelected == "Wiraswasta")
+            const Text("Pekerjaan : Wiraswasta"),
+          if (prov.enableDarkMode == true)
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                const Text('Cita-cita',
+                    style: TextStyle(fontWeight: FontWeight.bold)),
+                DropdownButton(
+                    items: items1.map((item) {
+                      return DropdownMenuItem(
+                        child: Text(item),
+                        value: item,
+                      );
+                    }).toList(),
+                    value: itemSelected1,
+                    onChanged: (val) {
+                      setState(() {
+                        itemSelected1 = val as String;
+                      });
+                    }),
+              ],
+            ),
+          if (itemSelected1 == "Programmer")
+            const DesProgrammer()
+          else if (itemSelected1 == "Dosen")
+            const DesDosen()
+          else if (itemSelected1 == "UI UX Designer")
+            const DesUiUx()
+          else if (itemSelected1 == "IT Consultant")
+            const DesITconsultant()
+        ],
+      ),
+    );
   }
+}
+
+class DesProgrammer extends StatelessWidget {
+  const DesProgrammer({super.key});
 
   @override
   Widget build(BuildContext context) {
-    final prov = Provider.of<Pertemuan07Provider>(context);
-    final List _body = [
-      const Pertemuan07Body(title: "Beranda"),
-      const Pertemuan07Body(title: "Chat"),
-      const Pertemuan07Body(title: "Profile"),
-    ];
-    return Scaffold(
-      floatingActionButton: FloatingActionButton(
-        child: const Icon(Icons.add),
-        onPressed: () {
-          prov.setTTLNotif = 1;
-        },
-      ),
-      appBar: AppBar(
-        leading: const Icon(Icons.menu),
-        title: const Text('Pertemuan07'),
-        actions: [
-          Stack(
-            alignment: Alignment.center,
-            children: [
-              const Icon(Icons.notifications),
-              Positioned(
-                  top: 10,
-                  right: 0,
-                  child: Container(
-                    width: 18,
-                    height: 18,
-                    decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(20),
-                      color: Colors.yellow,
-                    ),
-                    child: Text(
-                      prov.ttlNotif.toString(),
-                      textAlign: TextAlign.center,
-                      style: TextStyle(
-                          fontSize: 12,
-                          color: Colors.black,
-                          fontWeight: FontWeight.bold),
-                    ),
-                  ))
-            ],
-          ),
-          Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 16),
-            child: IconButton(
-              onPressed: () {
-                prov.resetNotif();
-              },
-              icon: const Icon(Icons.delete),
-            ),
-          ),
-          const Icon(Icons.more_vert)
-        ],
-        backgroundColor: Colors.purple,
-        elevation: 0,
-      ),
-      body: Padding(
-        padding: const EdgeInsets.all(20),
-        child: _body[_currentIndex],
-      ),
-      bottomNavigationBar: BottomNavigationBar(
-        onTap: onTabTapped,
-        currentIndex: _currentIndex,
-        items: [
-          BottomNavigationBarItem(icon: Icon(Icons.home), label: 'Home', ),
-          BottomNavigationBarItem(icon: Icon(Icons.mail), label: 'Chat'),
-          BottomNavigationBarItem(icon: Icon(Icons.person), label: 'Profile')
-        ],
-      ),
+    return Container(
+      child: Column(children: [
+        const Text("Cita-Cita : Programmer"),
+        Padding(padding: EdgeInsets.only(top: 40)),
+        Container(
+          padding: EdgeInsets.only(left: 1),
+          child: Column(children: [
+            Text('Deskripsi Programmer',
+                style: TextStyle(fontWeight: FontWeight.bold)),
+            Padding(padding: EdgeInsets.only(top: 5)),
+            Text("Programmer adalah ......"),
+          ]),
+        )
+      ]),
+    );
+  }
+}
+
+class DesDosen extends StatelessWidget {
+  const DesDosen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Column(children: [
+        const Text("Cita-Cita : Dosen"),
+        Padding(padding: EdgeInsets.only(top: 40)),
+        Container(
+          padding: EdgeInsets.only(left: 1),
+          child: Column(children: [
+            Text('Deskripsi Dosen',
+                style: TextStyle(fontWeight: FontWeight.bold)),
+            Padding(padding: EdgeInsets.only(top: 5)),
+            Text("Dosen adalah ......")
+          ]),
+        )
+      ]),
+    );
+  }
+}
+
+class DesUiUx extends StatelessWidget {
+  const DesUiUx({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Column(children: [
+        const Text("Cita-Cita : UI / UX Designer"),
+        Padding(padding: EdgeInsets.only(top: 40)),
+        Container(
+          padding: EdgeInsets.only(left: 1),
+          child: Column(children: [
+            Text('Deskripsi UI / UX Designer',
+                style: TextStyle(fontWeight: FontWeight.bold)),
+            Padding(padding: EdgeInsets.only(top: 5)),
+            Text("UI / UX Designer adalah ......")
+          ]),
+        )
+      ]),
+    );
+  }
+}
+
+class DesITconsultant extends StatelessWidget {
+  const DesITconsultant({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Column(children: [
+        const Text("Cita-Cita : IT Consultant"),
+        Padding(padding: EdgeInsets.only(top: 40)),
+        Container(
+          padding: EdgeInsets.only(left: 1),
+          child: Column(children: [
+            Text('Deskripsi IT Consultant',
+                style: TextStyle(fontWeight: FontWeight.bold)),
+            Padding(padding: EdgeInsets.only(top: 5)),
+            Text("IT Consultant adalah ......")
+          ]),
+        )
+      ]),
     );
   }
 }
